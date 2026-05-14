65caaf2df58408d46b39e52f952191158bd92996
Chloe-Hyebin-Kim <dev.5igner1101@gmail.com>
2026-05-15 05:38:52 +0900

    Vec3 전역

--- RollerCoasterSimulation/Vec3.cpp
+++ RollerCoasterSimulation/Vec3.cpp
@@ -48,3 +48,57 @@ Vec3& Vec3::operator+=(const Vec3& v)
     z += v.z;
     return *this;
 }
+
+Vec3 operator*(float s, const Vec3& v)
+{
+    return v * s;
+}
+
+float dot(const Vec3& a, const Vec3& b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+Vec3 cross(const Vec3& a, const Vec3& b)
+{
+    return Vec3(
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    );
+}
+
+float length(const Vec3& v)
+{
+    return std::sqrt(dot(v, v));
+}
+
+Vec3 normalize(const Vec3& v)
+{
+    const float len = length(v);
+
+    if (len < EPS)
+        return Vec3(0.0f, 1.0f, 0.0f);
+
+    return v / len;
+}
+
+float clampf(float v, float lo, float hi)
+{
+    return std::max(lo, std::min(hi, v));
+}
+
+Vec3 lerp(const Vec3& a, const Vec3& b, float t)
+{
+    return a * (1.0f - t) + b * t;
+}
+
+Vec3 rotateAroundAxis(const Vec3& v, const Vec3& axisUnit, float angle)
+{
+    const float c = std::cos(angle);
+    const float s = std::sin(angle);
+
+    return v * c
+        + cross(axisUnit, v) * s
+        + axisUnit * (dot(axisUnit, v) * (1.0f - c));
+}
--- RollerCoasterSimulation/Vec3.h
+++ RollerCoasterSimulation/Vec3.h
@@ -18,3 +18,13 @@ public:
 
     Vec3& operator+=(const Vec3& v);
 };
+
+Vec3 operator*(float s, const Vec3& v);
+
+float dot(const Vec3& a, const Vec3& b);
+Vec3 cross(const Vec3& a, const Vec3& b);
+float length(const Vec3& v);
+Vec3 normalize(const Vec3& v);
+float clampf(float v, float lo, float hi);
+Vec3 lerp(const Vec3& a, const Vec3& b, float t);
+Vec3 rotateAroundAxis(const Vec3& v, const Vec3& axisUnit, float angle);
